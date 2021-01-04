4e1873d65e9ff0fd6288901d5dd3ff85b8f2a50e
JottyGroups <bard143games@gmail.com>
2021-01-05 01:31:17 +0300

    Changed realization to work

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -1,18 +1,24 @@
 defmodule FizzBuzz do
     def get_fizzbuzz() do
         numbers = 1..100
-        Enum.each(numbers, fn x -> 
-            if (rem(x, 3) == 0) do IO.puts("Fizz") end
-            if(rem(x, 5) == 0) do IO.puts("Buzz") end
-            if (rem(x, 3) != 0 and rem(x, 5) != 0) do IO.puts(x) end
+        Enum.each(numbers, fn x ->
+            cond do
+                rem(x, 3) == 0 and rem(x, 5) == 0 -> IO.puts("FizzBuzz")
+                rem(x, 3) == 0 -> IO.puts("Fizz")
+                rem(x, 5) == 0 -> IO.puts("Buzz")
+                true -> IO.puts(Integer.to_string(x))
+            end
         end)
     end
 
     def get_fizzbuzz(numbers) do
-        Enum.each(numbers, fn x -> 
-            if (rem(x, 3) == 0) do IO.puts("Fizz") end
-            if(rem(x, 5) == 0) do IO.puts("Buzz") end
-            if (rem(x, 3) != 0 and rem(x, 5) != 0) do IO.puts(x) end
+        Enum.each(numbers, fn x ->
+            cond do
+                rem(x, 3) == 0 and rem(x, 5) == 0 -> IO.puts("FizzBuzz")
+                rem(x, 3) == 0 -> IO.puts("Fizz")
+                rem(x, 5) == 0 -> IO.puts("Buzz")
+                true -> IO.puts(Integer.to_string(x))
+            end
         end)
     end
 end
--- main.exs
+++ main.exs
@@ -1,7 +1,14 @@
 numbers = 1..100
 
-Enum.each(numbers, fn x -> 
-    if (rem(x, 3) == 0) do IO.puts("Fizz") end
-    if(rem(x, 5) == 0) do IO.puts("Buzz") end
-    if (rem(x, 3) != 0 and rem(x, 5) != 0) do IO.puts(x) end
-end)
+fizzbuzz = fn numbers ->
+    Enum.each(numbers, fn x ->
+        cond do
+            rem(x, 3) == 0 and rem(x, 5) == 0 -> IO.puts("FizzBuzz")
+            rem(x, 3) == 0 -> IO.puts("Fizz")
+            rem(x, 5) == 0 -> IO.puts("Buzz")
+            true -> IO.puts(Integer.to_string(x))
+        end
+    end)
+end
+
+fizzbuzz.(numbers)
